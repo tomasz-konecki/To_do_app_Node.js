7dd8ffb67295d13c2e643dc5863cf251fdc4035f
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-25 15:07:57 +0200

    Added connection to db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,36 @@
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var data = [
-  { item: "get milk" },
-  { item: "walk dog" },
-  { item: "kick some coding ass" }
-];
+//Conect to the database
+mongoose.connect("mongodb://test:test@ds119150.mlab.com:19150/todo");
+
+//Conect a schema (a blueprint)
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+//model (stored as a collection named 'Todo' in mongoDB)
+var Todo = mongoose.model("Todo", todoSchema);
+
+var itemOne = Todo({ item: "buy flowers" }).save(function(err) {
+  if (err) throw err;
+  console.log("Item saved");
+});
+
+// var data = [
+//   { item: "get milk" },
+//   { item: "walk dog" },
+//   { item: "kick some coding ass" }
+// ];
 
 module.exports = function(app) {
   app.get("/todo", function(req, res) {
-    res.render("todo", { todos: data });
+    //get data frm mongodb and pass it to view
+    Todo.find({}, function(err, data) {
+      if (err) throw err;
+      res.render("todo", { todos: data });
+    });
   });
 
   app.post("/todo", urlencodedParser, function(req, res) {
