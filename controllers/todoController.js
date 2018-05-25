9060aa48b0b109375bd4d3c8eb60cd918b8dc43e
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-25 13:49:37 +0200

    Updated code

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,9 +1,27 @@
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var data = [
+  { item: "get milk" },
+  { item: "walk dog" },
+  { item: "kick some coding ass" }
+];
+
 module.exports = function(app) {
   app.get("/todo", function(req, res) {
-    res.render("todo");
+    res.render("todo", { todos: data });
   });
 
-  app.post("/todo", function(req, res) {});
+  app.post("/todo", urlencodedParser, function(req, res) {
+    console.log(req.body);
+    data.push(req.body);
+    res.json(data);
+  });
 
-  app.delete("/todo", function(req, res) {});
+  app.delete("/todo/:item", function(req, res) {
+    data = data.filter(function(todo) {
+      return todo.item.replace(/\s/g, "") !== req.params.item;
+    });
+    res.json(data);
+  });
 };
